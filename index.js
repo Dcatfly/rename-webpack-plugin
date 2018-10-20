b927cc337006ff4405f76db3c68db62a373aa9f1
dcatfly <772736731@qq.com>
2018-10-20 14:24:23 +0800

    add prettier config and format code

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 module.exports = class Rename {
   constructor(options) {
-    this.options = {...options};
+    this.options = { ...options };
   }
 
   apply(compiler) {
     compiler.hooks.emit.tap('Rename.emit', compilation => {
-      const {assets} = compilation;
-      const {originNameReg, targetName} = this.options;
-      const originFiles = Object.keys(assets).filter(fileName => new RegExp(originNameReg).test(fileName));
+      const { assets } = compilation;
+      const { originNameReg, targetName } = this.options;
+      const originFiles = Object.keys(assets).filter(fileName =>
+        new RegExp(originNameReg).test(fileName)
+      );
 
       originFiles.forEach(fileName => {
         assets[fileName.replace(originNameReg, targetName)] = assets[fileName];
         delete assets[fileName];
       });
-
     });
   }
 };
