4960a5a2ec5fda3c8d615b4dde972ff7541ceb08
Stan <121045600+stanisnotavailable@users.noreply.github.com>
2023-01-26 17:49:37 +0200

    Update compiler hook to webpack 5 API
    The compiler.hooks.compilation.tap method is used to attach a new hook to the compilation phase of the Webpack build process. In this case, the hook is called "Rename.emit" and the callback function that is passed to it is executed when the compilation phase starts.
    
    The callback function uses the compilation.hooks.optimizeAssets.tapAsync method to attach another hook to the optimizeAssets phase of the compilation process. This hook is called "Rename.emit" and it's a async hook, which means it takes a callback function that should be called when the hook is finished.
    
    The callback function takes two arguments: assets, the assets object, and callback which is a function that should be called when the hook is finished.

--- index.js
+++ index.js
@@ -4,17 +4,25 @@ module.exports = class Rename {
   }
 
   apply(compiler) {
-    compiler.hooks.emit.tap('Rename.emit', compilation => {
-      const { assets } = compilation;
-      const { originNameReg, targetName } = this.options;
-      const originFiles = Object.keys(assets).filter(fileName =>
-        new RegExp(originNameReg).test(fileName)
-      );
+    compiler.hooks.compilation.tap("Rename.emit", (compilation) => {
+      compilation.hooks.optimizeAssets.tapAsync(
+        "Rename.emit",
+        (assets, callback) => {
+          // Modify assets object here
+          const { originNameReg, targetName } = this.options;
+          const originFiles = Object.keys(assets).filter((fileName) =>
+            new RegExp(originNameReg).test(fileName)
+          );
 
-      originFiles.forEach(fileName => {
-        assets[fileName.replace(originNameReg, targetName)] = assets[fileName];
-        delete assets[fileName];
-      });
+          originFiles.forEach((fileName) => {
+            assets[fileName.replace(originNameReg, targetName)] =
+              assets[fileName];
+            delete assets[fileName];
+          });
+
+          callback();
+        }
+      );
     });
   }
 };
